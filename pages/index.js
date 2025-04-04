377154dc87e77bdfe0e7e77af274b9df4e826388
Attila Horvath <sweeptic@gmail.com>
2025-04-04 16:04:10 +0200

    021 How NextJS Page Pre-Rendering Actually Works

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,38 @@
+import { useState } from 'react';
+
 import MeetupList from '../components/meetups/MeetupList';
 
-export default function HomePage() {
-  const DUMMY_MEETUPS = [
-    {
-      id: 'm1',
-      title: 'A First Meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Some address 5, 12345 Some City',
-      description: 'This is a first meetup!',
-    },
-    {
-      id: 'm2',
-      title: 'A Second Meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Some address 5, 12345 Some City',
-      description: 'This is a second meetup!',
-    },
-  ];
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a second meetup!',
+  },
+];
 
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+export default function HomePage() {
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  //   useEffect(() => {
+  //     setLoadedMeetups(DUMMY_MEETUPS);
+  //   }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
+}
+
+export async function getServerSideProps(ctx) {
+  return {
+    props: {
+      data: null,
+    },
+  };
 }
