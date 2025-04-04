059da4af4a4187d9193cbbac04b070c288e76746
Attila Horvath <sweeptic@gmail.com>
2025-04-04 17:21:37 +0200

    025 Working With Dynamic Path Params In getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,19 +2,33 @@ import { useRouter } from 'next/router';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-export default function MeetupDetails() {
+export default function MeetupDetails(props) {
   const router = useRouter();
 
   console.log('router', router.query.meetupId);
 
   return (
     <>
-      <MeetupDetail
-        image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
-        title="picture alt here"
-        address="Some address 5 4432"
-        description="Te meetup description"
-      />
+      <MeetupDetail {...props.meetupData} />
     </>
   );
+}
+
+export async function getStaticProps(ctx) {
+  // context.params !!! holds the URL
+
+  const meetupId = context.params.meetupId;
+  console.log('meetupId', meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        // id: m1,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+        title: 'picture alt here',
+        address: 'Some address 5 4432',
+        description: 'Te meetup description',
+      },
+    },
+  };
 }
